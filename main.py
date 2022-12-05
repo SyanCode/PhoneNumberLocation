63b36a0f8d14ccdf0ad00f24998d879a571cf8da
DeffyCode <hilairebenamorsyan@gmail.com>
2022-12-05 22:50:37 +0100

    Commentaires ajoutés et tri des lignes

--- main.py
+++ main.py
@@ -16,11 +16,9 @@ import folium
 num = input("Entrer un numéro de téléphone français (format +33) : ")
 monNum = phonenumbers.parse(num)
 localisation = geocoder.description_for_number(monNum,"fr")
-print("Pays : " + localisation)
 
-# Trouver l'opérateur
+# Trouver l'opérateur du numéro
 operator = phonenumbers.parse(num)
-print("Opérateur : " + carrier.name_for_number(operator,"fr"))
 
 # Trouver la latitude/longitude
 key = api_key_opencage
@@ -29,13 +27,21 @@ request = str(localisation)
 answer = coords.geocode(request)
 lat = answer[0]["geometry"]["lat"]
 lng = answer[0]["geometry"]["lng"]
-print("Latitude : " + str(lat))
-print("Longitude : " + str(lng))
 
 # Création de la Map
 Map = folium.Map(location=[lat, lng], zoom_start=12)
 folium.Marker([lat, lng], popup=localisation).add_to(Map)
-Map.save("map" + num + ".html")
 
-# Message de fin
-print("La map a été chargée dans le projet en format html.")
+# Prints des informations
+
+# Pays
+print("Pays : " + localisation)
+# Opérateur
+print("Opérateur : " + carrier.name_for_number(operator,"fr"))
+# Latitude/Longitude
+print("Latitude : " + str(lat))
+print("Longitude : " + str(lng))
+# Map save
+Map.save("map" + num + ".html")
+# Print à titre informatif
+print("La map a été chargée dans le projet sous le nom de map" + num + ".html.")
